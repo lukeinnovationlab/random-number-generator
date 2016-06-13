d480e157ee9bf320652f15857a23f001fea6bd25
Luke Chen <lchen@blackberry.com>
2016-06-13 14:13:00 -0400

    Implementation of random number generator

--- app/src/main/java/com/lukeinnovationlab/randomnumbergenerator/MainActivity.java
+++ app/src/main/java/com/lukeinnovationlab/randomnumbergenerator/MainActivity.java
@@ -1,13 +1,39 @@
 package com.lukeinnovationlab.randomnumbergenerator;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+    private static final Random RANDOM = new Random(System.currentTimeMillis());
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 999;
+
+    private TextView mViewRandomNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mViewRandomNumber = (TextView) findViewById(R.id.text_random_number);
+    }
+
+    protected void onGenerate(View view) {
+        if (view.getId() == R.id.action_generate) {
+            int number = generateRandomNumber();
+            mViewRandomNumber.setText(String.valueOf(number));
+
+            Log.i(TAG, "Generated: " + number);
+        }
+    }
+
+    private int generateRandomNumber() {
+        return RANDOM.nextInt(MAX_NUMBER) + MIN_NUMBER;
     }
 }
